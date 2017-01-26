2c45b747f673554803ca61810ffbac00321f4d84
Tomasz Knapik <tmkn0808@gmail.com>
2017-01-26 12:34:44 +0000

    Add dashboard route

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,37 @@
 import { NgModule }      from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { PageNotFoundComponent } from './not-found.component';
+import { DashboardComponent } from './companion/dashboard.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    PageNotFoundComponent
+  ],
   bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
