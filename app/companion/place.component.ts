5aa237921110ccd72ff6b2311805701761000d96
Tomasz Knapik <tmkn0808@gmail.com>
2017-01-30 01:34:59 +0000

    Add place detail page

--- app/companion/place.component.ts
+++ app/companion/place.component.ts
@@ -1,11 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location }                 from '@angular/common';
+import { ActivatedRoute, Params }   from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
+import { CompanionService } from './companion.service';
 import { Place } from './place';
 
 @Component({
   moduleId: module.id,
   templateUrl: './place.component.html',
   selector: 'place'
-}) export class PlaceComponent {
-  @Input() category: Place;
+})
+export class PlaceComponent implements OnInit {
+  place: Place;
+
+  constructor(
+    private service: CompanionService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    this.route.params
+    .switchMap((params: Params) => this.service.getPlace(+params['id']))
+    .subscribe(place => this.place = place);
+  }
 }
--- app/companion/placeCategory.component.ts
+++ app/companion/placeCategory.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
+import { ActivatedRoute, Params }   from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 
 import { CompanionService } from './companion.service';
