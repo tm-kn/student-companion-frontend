c1da012c7fd30a468adab6eb5f247c25547eb77d
Tomasz Knapik <tmkn0808@gmail.com>
2017-01-30 01:27:33 +0000

    Add place category detail page

---
+++ app/companion/placeCategory.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { CompanionService } from './companion.service';
+import { PlaceCategory } from './placeCategory';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: './placeCategory.component.html',
+  selector: 'place-category'
+})
+export class PlaceCategoryComponent implements OnInit {
+  category: PlaceCategory;
+
+  constructor(
+    private service: CompanionService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    this.route.params
+    .switchMap((params: Params) => this.service.getCategory(+params['id']))
+    .subscribe(category => this.category = category);
+  }
+}
--- app/companion/placeCategory.ts
+++ app/companion/placeCategory.ts
@@ -1,7 +1,10 @@
+import { Place } from './place';
+
 export class PlaceCategory {
   id: number;
   name: string;
   slug: string;
   parent: PlaceCategory;
   category_children: PlaceCategory[];
+  places: Place[];
 }
