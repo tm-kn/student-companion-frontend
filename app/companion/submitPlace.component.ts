1fa1d73095bc2da5e70f36d84757e6269d6a6928
Tomasz Knapik <tmkn0808@gmail.com>
2017-02-07 00:49:49 +0000

    Add empty submit place page and change login/register link to the routerlinks

---
+++ app/companion/submitPlace.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'submit-place',
+  templateUrl: './submitPlace.component.html'
+})
+export class SubmitPlaceComponent {}
