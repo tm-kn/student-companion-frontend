673086d3681fb1e5e3f28884b0f6e1a83f03e77a
Tomasz Knapik <tmkn0808@gmail.com>
2017-02-14 15:04:41 +0000

    Add empty login and register pages

---
+++ app/companion/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: './login.component.html',
+  selector: 'login'
+})
+export class LoginComponent {}
---
+++ app/companion/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: './register.component.html',
+  selector: 'register'
+})
+export class RegisterComponent {}
