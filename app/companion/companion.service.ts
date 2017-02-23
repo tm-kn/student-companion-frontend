a24bb83b83bca54d8753b69e70ca74889eb99c2c
Tomasz Knapik <tmkn0808@gmail.com>
2017-02-23 13:32:44 +0000

    Add authenticate endpoint

--- app/companion/companion.service.ts
+++ app/companion/companion.service.ts
@@ -1,39 +1,51 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, URLSearchParams } from '@angular/http';
+import { Http, Headers, RequestOptions, Response, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import { AppConfig } from '../app.config';
 import { Place } from './place';
 import { PlaceCategory } from './placeCategory';
+import { User } from './user';
 
 @Injectable()
 export class CompanionService {
   private baseUrl = AppConfig.API_ENDPOINT;
   private placesUrl = this.baseUrl + 'places/';
   private categoriesUrl = this.baseUrl + 'place-categories/';
+  private authenticateUrl = this.baseUrl + 'api-token-auth/';
 
   constructor(private http: Http) {}
 
+  authenticate(username: string, password: string): Observable<User> {
+    return this.http.post(
+                      this.authenticateUrl,
+                      { username, password },
+                      this.getOptions()
+                    )
+                    .map((response: Response) => response.json()['user'])
+                    .catch(this.handleError);
+  }
+
   getPlaces(): Observable<Place[]> {
-    return this.http.get(this.placesUrl)
+    return this.http.get(this.placesUrl, this.getOptions())
                     .map(this.extractData)
                     .catch(this.handleError);
   }
 
   getPlace(id: number): Observable<Place> {
-    return this.http.get(this.placesUrl + id + '/')
+    return this.http.get(this.placesUrl + id + '/', this.getOptions())
                     .map(this.extractData)
                     .catch(this.handleError);
   }
 
   getCategories(): Observable<PlaceCategory[]> {
-    return this.http.get(this.categoriesUrl)
+    return this.http.get(this.categoriesUrl, this.getOptions())
                     .map(this.extractData)
                     .catch(this.handleError);
   }
 
   getCategory(id: number): Observable<PlaceCategory> {
-    return this.http.get(this.categoriesUrl + id + '/')
+    return this.http.get(this.categoriesUrl + id + '/', this.getOptions())
                     .map(this.extractData)
                     .catch(this.handleError);
   }
@@ -49,7 +61,7 @@ export class CompanionService {
                       student_discount: studentDiscount,
                       categories: categories,
                       description: description
-                    })
+                    }, this.getOptions())
                     .map(this.extractData)
                     .catch(this.handleError);
   }
@@ -61,6 +73,37 @@ export class CompanionService {
     return this.http.get(this.placesUrl, { search: params })
                     .map(this.extractData)
                     .catch(this.handleError);
+  }
+
+  private getHeaders(): Headers {
+    let headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+
+    const token = this.getToken();
+
+    if (token) {
+      headers['Authentication'] = 'JWT ' + token;
+    }
+
+    return new Headers(headers);
+  }
+
+  private getToken(): string {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      return token;
+    }
+
+    return undefined;
+  }
+
+  private getOptions(): RequestOptions {
+    const options = new RequestOptions({ headers: this.getHeaders() });
+    console.log(options);
+    return options;
   }
 
   private extractData(response: Response) {
