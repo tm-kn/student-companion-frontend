8a3800de04d3c04c97c455e336b1b3cc120aa78e
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2017-03-13 19:11:32 +0000

    Use HTTPS

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
-  public static API_ENDPOINT_DEV = 'http://localhost:8000/';
-  public static API_ENDPOINT_PROD = 'http://django.sc.tmkn.uk/'
+  public static API_ENDPOINT_DEV = '//localhost:8000/';
+  public static API_ENDPOINT_PROD = '//django.sc.tmkn.uk/'
   public static LOCATION_LATITUDE = 53.6458;
   public static LOCATION_LONGITUDE = -1.7850;
 }
